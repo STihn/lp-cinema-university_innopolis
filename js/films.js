4a39d45946204ebbe31b074e986d7e091076e983
STihn <you.lucky@bk.ru>
2019-11-07 16:42:31 +0300

    4.4-start2

--- js/films.js
+++ js/films.js
@@ -58,24 +58,64 @@
 
 
 
-let ganar = ['фантастика', 'Боевик', 'Приключения', 'Фэнтези', 'Драма', 'Мультфильм', 'Комедия']
+let ganar = [
+    'фантастика',   //0
+    'Боевик',       //1
+    'Приключения',  //2
+    'Фэнтези',      //3
+    'Драма',        //4
+    'Мультфильм',   //5
+    'Комедия'       //6
+    ]
 
 let films = [
-    ['10:00', 'Человек-паук',],
-    ['12:00', 'Собачья жизнь 2',],
-    ['14:00', 'История игрушек 4',],
-    ['16:00', 'Люди в черном: Интернэшнл',]
+    ['10:00', 'Человек-паук',[0,1,2]],
+    ['12:00', 'Собачья жизнь 2',[3,4,5]],
+    ['14:00', 'История игрушек 4',[6,0,1]],
+    ['16:00', 'Люди в черном: Интернэшнл',[2,3,4]]
 ]
+// Определяем длину films
+console.log(films.length);
 
-for(var i = 0; i < films.length; i++){
-    console.log(films[0][i] + ' ' + films[1][i] + ' ' + films[2][i] + ' ' + films[3][i]);
-   }
+//Пишем цикл for по массиву films
+for (var i=0; i<films.length; i++) {
+    console.log(films)
+    // for (var i=0; i<films.length; i++) {
+    //     const ganar  = films[i][2].split('/');
+    //         console.log(ganar)
+        
+    // }
 
+ }
+ for (var j=0; j < ganar.length; j++){
+    console.log(ganar)
+}
+
+ for (let i = 0; i < films.length; i++){
+    ganar_films = []
+    for (let j = 0; j < (films[i][2]).length; j++) {
+        ganar_films.push(ganar[films[i][2][j]])
+    }
+    ganar_films.push(ganar_films)
+    console.log(ganar_films)
+}
+// for(var i = 0; i < ganar_films.length; i++){
+//     console.log(ganar_films[0][i] + ' ' + films[1][i] + ' ' + films[2][i] + ' ' + films[3][i]);
+//    }
+
+    
+// for (var j=0; j < ganres_indexes.length; j++){}
 // let ganar_films_1 = films[0].concat(ganar[0],ganar[1],ganar[2]);
 // console.log(ganar_films_1)
 // films[0].push(ganar[0],ganar[1], ganar[3]);
 // console.log(films[0])
 
-// console.log(films[0].length)
-let element_start_film_1 = document.getElementById('start_film_1')
-element_start_film_1.innerHTML = films[0][0]
+ 
+let element_start_film_1 = document.getElementById('start_film_1');
+element_start_film_1.innerHTML = films[0][0];
+
+let element_name_film_1 = document.getElementById('name_film_1');
+element_name_film_1.innerHTML = films[0][1];
+
+let element_ganar_film_1 = document.getElementById('ganar_film_1');
+element_ganar_film_1.innerHTML = ganar_films[3];
